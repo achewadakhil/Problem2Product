5dfe1e3e751a7058fac057bc9aa74a568330923c
achewadakhil <achewadakhil11@gmail.com>
2026-04-24 13:45:59 +0530

    Initial commit

---
+++ backend/src/main/java/com/Problem2Product/Problem2ProductApplication.java
@@ -0,0 +1,13 @@
+package com.Problem2Product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problem2ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Problem2ProductApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Problem2Product/Problem2ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Problem2Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Problem2ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
